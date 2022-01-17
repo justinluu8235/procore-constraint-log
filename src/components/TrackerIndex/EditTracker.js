ad92e6179e0ca4ace914866be23410a749840252
justinluu8235 <justinluu8235@gmail.com>
2022-01-17 15:52:14 -0800

    mobile

--- src/components/TrackerIndex/EditTracker.js
+++ src/components/TrackerIndex/EditTracker.js
@@ -100,73 +100,73 @@ class EditTracker extends Component {
         return (
             <div>
                 <Navbar />
-            
-            <div className="edit-tracker">
-                
 
-                <h1 className="edit-tracker-title">Edit Constraint Tracker</h1>
+                <div className="edit-tracker">
 
-                <div className="head-label-container">
-                    <span className="label-text">General</span>
+
+                    <h1 className="edit-tracker-title">Edit Constraint Tracker</h1>
+
+                    <div className="head-label-container">
+                        <span className="label-text">General</span>
+                    </div>
+
+                    <form action={formLink} method="POST">
+                        <div className="new-tracker-container">
+                            <div className="general-info-container">
+                                <div>General Information</div>
+                            </div>
+
+                            <div className="new-tracker-name-container">
+                                <span className="tracker-name-field-text">Tracker Name:</span>
+
+                                <div className="tracker-name-input-container">
+                                    <input className="tracker-name-input" type="text" name="trackerName" value={this.state.trackerNameVal} onChange={this.handleChange}></input>
+                                </div>
+                            </div>
+
+                            <div className="new-tracker-group-container-5">
+                                <span className="tracker-group-field-text-1">Group Members:</span>
+                                <div className="group-members-container-edit" id="all-members">
+                                    <div className="individual-member-container-edit">
+                                        <div class="edit-member-row">
+                                            <span className="tracker-group-field-text">Name: </span>
+                                            <input className="member-name-input" type="text" name="memberName" value={this.state.firstNameInput} onChange={this.handleChange}></input>
+                                        </div>
+                                        <div class="edit-member-row">
+                                            <span className="tracker-group-field-text">Company: </span>
+                                            <input className="member-company-input" type="text" name="memberCompany" value={this.state.firstCompanyInput} onChange={this.handleChange}></input>
+                                        </div>
+                                    </div>
+                                    {this.displayMembers()}
+                                    {this.state.group}
+
+                                    <div className="individual-member-container-edit" id="member-fields">
+                                        <div class="edit-member-row">
+                                            <span className="tracker-group-field-text">Name: </span>
+                                            <input className="member-name-input" type="text" name="memberName"></input>
+                                        </div>
+                                        <div class="edit-member-row">
+                                            <span className="tracker-group-field-text">Company: </span>
+                                            <input className="member-company-input" type="text" name="memberCompany"></input>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <input type="button" name="button" className="add-button" value="Add another Member" id="addMemberButton" onClick={this.handleClick} />
+                            </div>
+
+                        </div>
+
+                        <div className="submission-footer">
+                            <div className="discard-create-container">
+
+                                <input type="submit" className="create-button-2" value="Update" />
+
+                            </div>
+                        </div>
+                    </form>
+
                 </div>
-
-                <form action={formLink} method="POST">
-                    <div className="new-tracker-container">
-                        <div className="general-info-container">
-                            <div>General Information</div>
-                        </div>
-
-                        <div className="new-tracker-name-container">
-                            <span className="tracker-name-field-text">Tracker Name:</span>
-
-                            <div className="tracker-name-input-container">
-                                <input className="tracker-name-input" type="text" name="trackerName" value={this.state.trackerNameVal} onChange={this.handleChange}></input>
-                            </div>
-                        </div>
-
-                        <div className="new-tracker-group-container-5">
-                            <span className="tracker-group-field-text-1">Group Members:</span>
-                            <div className="group-members-container-edit" id="all-members">
-                                <div className="individual-member-container-edit">
-                                    <div class="edit-member-row">
-                                        <span className="tracker-group-field-text">Name: </span>
-                                        <input className="member-name-input" type="text" name="memberName" value={this.state.firstNameInput} onChange={this.handleChange}></input>
-                                    </div>
-                                    <div class="edit-member-row">
-                                        <span className="tracker-group-field-text">Company: </span>
-                                        <input className="member-company-input" type="text" name="memberCompany" value={this.state.firstCompanyInput} onChange={this.handleChange}></input>
-                                    </div>
-                                </div>
-                                {this.displayMembers()}
-                                {this.state.group}
-
-                                <div className="individual-member-container-edit" id="member-fields">
-                                    <div class="edit-member-row">
-                                        <span className="tracker-group-field-text">Name: </span>
-                                        <input className="member-name-input" type="text" name="memberName"></input>
-                                    </div>
-                                    <div class="edit-member-row">
-                                        <span className="tracker-group-field-text">Company: </span>
-                                        <input className="member-company-input" type="text" name="memberCompany"></input>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <input type="button" name="button" className="add-button" value="Add another Member" id="addMemberButton" onClick={this.handleClick} />
-                        </div>
-
-                    </div>
-
-                    <div className="submission-footer">
-                        <div className="discard-create-container">
-
-                            <input type="submit" className="create-button-2" value="Update" />
-
-                        </div>
-                    </div>
-                </form>
-
-            </div>
             </div>
         )
     }
